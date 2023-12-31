81d803ce10e9f4d17eb32f56342d00fb385a4357
kurutonss <kurutoarai@icloud.com>
2023-12-31 15:00:23 +0900

    fix exam4

--- src/care.java
+++ src/care.java
@@ -1,23 +1,23 @@
-public class care {
+public class Care {
     private String name;
     private String temperture;
     private int price;
 
-    public care(String name, String temperture, int price) {
+    public Care(String name, String temperture, int price) {
         this.name = name;
         this.temperture = temperture;
         this.price = price;
     }
 
-    public String getname() {
+    public String getName() {
         return name;
     }
 
-    public String gettemperture() {
+    public String getTemperture() {
         return temperture;
     }
 
-    public int getprice() {
+    public int getPrice() {
         return price;
     }
 }
--- src/main.java
+++ src/main.java
@@ -1,25 +1,25 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class main {
+public class Main {
     public static void main(String[] args) {
-        care drink1 = new care("hot coffee", "hot", 300);
-        care drink2 = new care("ice coffee", "cold", 300);
-        care drink3 = new care("special coffee", "hot", 500);
-        care drink4 = new care("wow coffee", "apollon", 1000000000);
+        Care drink1 = new Care("hot coffee", "hot", 300);
+        Care drink2 = new Care("ice coffee", "cold", 300);
+        Care drink3 = new Care("special coffee", "hot", 500);
+        Care drink4 = new Care("wow coffee", "apollon", 1000000000);
 
-        List<care> menulist = new ArrayList<>();
+        List<Care> menulist = new ArrayList<>();
         menulist.add(drink1);
         menulist.add(drink2);
         menulist.add(drink3);
         menulist.add(drink4);
 
         menulist.stream()
-                .filter(care -> care.gettemperture().equals("apollon"))
-                .forEach(care -> {
+                .filter(Care -> Care.getTemperture().equals("apollon"))
+                .forEach(Care -> {
                     System.out.println("^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^");
-                    System.out.println("メニュー名：" + care.getname());
-                    System.out.println("値段：" + care.getprice());
+                    System.out.println("メニュー名：" + Care.getName());
+                    System.out.println("値段：" + Care.getPrice());
                     System.out.println("^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^");
                 });
     }
